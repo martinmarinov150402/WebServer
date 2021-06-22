9bd467d0e08319d7ea095e74f3a7eae8164ad0d2
Martin Marinov <martinmarinov150402@gmail.com>
2021-06-22 23:44:41 +0300

    End of day 22.06.2021

--- server.cpp
+++ server.cpp
@@ -11,6 +11,8 @@
 #include <iostream>
 #include "router.cpp"
 #include <map>
+#include <vector>
+#include <thread>
 
 #define PORT 3000
 
@@ -37,8 +39,57 @@ void initializeRouter(Router* router)
     void (*helloPtr)(map<string,string>) = &helloCustomRoute;
     insertRoute(router,"POST:/testCustomRoute",helloPtr);   
 }
+void serve(int sockId,int sock2,Router* router)
+{
+    string reply="";
+    int valReader;
+        char buffer[4096]= {0};
+        valReader = read(sock2, buffer, 1024);
+        recv(new_connection, (char*)&buffer, sizeof(buffer), 0);
+        pair<string,string>metPath = getMethodAndPath(buffer);
+    if(metPath.first!="GET")
+    {
+        map<string,string>a;
+        int res = useRoute(router,metPath.first+":"+metPath.second,a);
+        if(res==404)
+        {
+            string rep = notFoundReply();
+            send(sockId,rep.c_str(),strlen(rep.c_str()),0);
+        }
+        reply = notFoundReply();
+    }
+    else
+    {
+        reply = "";
+        if(metPath.second=="/")
+        {
+            metPath.second="index.html";
+
+        }
+        else
+        {
+            metPath.second = metPath.second.substr(1,metPath.second.size()-1);
+        }
+        string index = readFile(metPath.second);
+        int ContentLength = index.size();
+        ostringstream CLSS;
+        CLSS<<ContentLength;
+        if(index!="")
+        {
+            reply=okHeaders(CLSS.str())+index;
+        }
+        else
+        {
+            reply=notFoundReply();   
+        }
+    }
+    printf("%s\n",buffer);
+    send(sockId, reply.c_str(), strlen(reply.c_str()),0);
+    close(sockId);
+}
 int main()
 {
+    vector<thread> threads;
     int sock = socket(AF_INET, SOCK_STREAM, 0);
     struct sockaddr_in address;
     int opt=1;
@@ -55,7 +106,7 @@ int main()
         perror("Can't bind");
         exit(EXIT_FAILURE);
     }
-    string reply="";
+    
     while(1)
     {
 
@@ -70,56 +121,18 @@ int main()
             perror("Accept error");
             exit(EXIT_FAILURE);
         }
-        int valReader;
-        char buffer[4096]= {0};
-        valReader = read(sock, buffer, 1024);
-        recv(new_connection, (char*)&buffer, sizeof(buffer), 0);
-        pair<string,string>metPath = getMethodAndPath(buffer);
+
+        
         if(new_connection)
         {
+            thread tmpTh(serve,new_connection,sock,router);
             //Controller* cont = new Controller(new_connection,router);
-            if(metPath.first!="GET")
-            {
-                map<string,string>a;
-                int res = useRoute(router,metPath.first+":"+metPath.second,a);
-                if(res==404)
-                {
-                    string rep = notFoundReply();
-                    send(new_connection,rep.c_str(),strlen(rep.c_str()),0);
-                }
-                reply = notFoundReply();
-            }
-            else
-            {
-                reply = "";
-                if(metPath.second=="/")
-                {
-                    metPath.second="index.html";
-
-                }
-                else
-                {
-                    metPath.second = metPath.second.substr(1,metPath.second.size()-1);
-                }
-                string index = readFile(metPath.second);
-                int ContentLength = index.size();
-                ostringstream CLSS;
-                CLSS<<ContentLength;
-                if(index!="")
-                {
-                    reply=okHeaders(CLSS.str())+index;
-                }
-                else
-                {
-                    reply=notFoundReply();   
-                }
-            }
+            tmpTh.join();
+            
+            
 
         }
-        printf("%s\n",buffer);
-
-        send(new_connection, reply.c_str(), strlen(reply.c_str()),0);
-        close(new_connection);
+        
         //continue;
     }
 
